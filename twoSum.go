804c37f18b9fa9063535c523d7ff61dd30217753
lynt11 <15754311153@163.com>
2018-12-25 20:37:28 +0800

    给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
    你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
    
    示例:
    
    给定 nums = [2, 7, 11, 15], target = 9
    
    因为 nums[0] + nums[1] = 2 + 7 = 9
    所以返回 [0, 1]
    
    方法二   哈希表遍历1次时间复杂度 O(n)

--- twoSum.go
+++ twoSum.go
@@ -1,19 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func twoSum(nums []int, target int) []int {
 	maps := make(map[int]int)
 	for i, v := range nums {
+		temp := target - v
+		if i2, isok := maps[temp]; isok {
+			return []int{i, i2}
+		}
 		maps[v] = i
 	}
-	for i, v := range nums {
-		temp := target - v
-		if _, isok := maps[temp]; isok && maps[temp] != i {
-			return []int{i, maps[temp]}
-		}
-	}
 	return []int{-1, -1}
+
 }
 func main() {
 	nums := []int{2, 7, 11, 15}
